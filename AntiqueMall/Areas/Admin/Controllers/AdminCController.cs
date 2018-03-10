3eb7b23f0c2cff03f3d25f681d2247a1cc8f1010
Rizvan Haciyev <haciyev_9022@mail.ru>
2018-03-10 23:20:08 +0400

    add

---
+++ AntiqueMall/Areas/Admin/Controllers/AdminCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AntiqueMall.Areas.Admin.Controllers
+{
+    public class AdminCController : Controller
+    {
+        // GET: Admin/AdminC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
